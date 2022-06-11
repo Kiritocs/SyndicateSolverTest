e14acdfa1b954b5353f0f89a29a4aa28e622b91a
Kiritocs <81497153+Kiritocs@users.noreply.github.com>
2022-06-11 11:31:56 +0200

    Initial commit

---
+++ Core.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ExileCore;
+using ExileCore.PoEMemory;
+using ExileCore.PoEMemory.Elements;
+using ExileCore.Shared.Enums;
+using SharpDX;
+
+namespace SyndicateSolver
+{
+    public class Core : BaseSettingsPlugin<Settings>
+    {
+
+        private static bool betrayalWindowOpen = false;
+        private static bool syndicatePanelOpen = false;
+
+        public override void Render()
+        {
+            Element betrayalWindow = GameController.Game.IngameState.IngameUi.BetrayalWindow;
+            if (betrayalWindow.IsVisibleLocal)
+                LogMessage("BetrayalWindow visible");
+            if (betrayalWindow.IsVisibleLocal != betrayalWindowOpen)
+            {
+                betrayalWindowOpen = !betrayalWindowOpen;
+                LogMessage("BetrayalWindow is " + (betrayalWindowOpen ? "visible" : "not visible"));
+                if (betrayalWindowOpen)
+                {
+                    IterateChildren(betrayalWindow);
+                }
+            }
+            Element syndicatePanel = GameController.Game.IngameState.IngameUi.SyndicatePanel;
+            if (syndicatePanel.IsVisibleLocal)
+                LogMessage("syndicatePanel visible");
+            if (syndicatePanel.IsVisibleLocal != syndicatePanelOpen)
+            {
+                syndicatePanelOpen = !syndicatePanelOpen;
+                LogMessage("SyndicatePanel is " + (syndicatePanelOpen ? "visible" : "not visible"));
+                if (syndicatePanelOpen)
+                {
+                    IterateChildren(syndicatePanel);
+                }
+            }
+            Element syndicateTree = GameController.Game.IngameState.IngameUi.SyndicateTree;
+            if (syndicateTree.IsVisibleLocal)
+                LogMessage("syndicateTree visible");
+            if (syndicateTree.IsVisibleLocal != syndicateTreeOpen)
+            {
+                syndicateTreeOpen = !syndicateTreeOpen;
+                LogMessage("SyndicateTree is " + (syndicatePanelOpen ? "visible" : "not visible"));
+                if (syndicateTreeOpen)
+                {
+                    IterateChildren(syndicateTree);
+                }
+            }
+        }
+
+        private void IterateChildren(Element element)
+        {
+            Element aisling = element.GetElementByString("Aisling");
+            if (aisling != null)
+            {
+                Graphics.DrawText("Aisling found below", new Vector2(aisling.GetClientRectCache.Center.X, aisling.GetClientRectCache.Center.Y-10), Color.White, FontAlign.Center);
+                Graphics.DrawFrame(aisling.GetClientRectCache, Color.AliceBlue, 2);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# SyndicateSolver
+Using this project to learn C#. This will eventually 'solve' syndicate / betrayal encounters for you by telling you which options to pick (bargain, execute or neither) to reach your desired goal in the settings
